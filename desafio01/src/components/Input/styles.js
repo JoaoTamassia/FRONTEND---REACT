928a06c2c752fcd5b2f2174b05c8a53d5d583f55
JoaoTamassia <joaotamassia@alunos.utfpr.edu.br>
2024-12-09 11:29:47 -0300

    Calculadora

---
+++ desafio01/src/components/Input/styles.js
@@ -0,0 +1,32 @@
+import styled from 'styled-components';
+
+export const InputContainer = styled.div`
+    width: 100%;
+    height: 75px;
+    background-color: #AAFFAA;
+
+    display: flex;
+    align-items: center;
+    justify-content: flex-end;
+
+    font-size: 24px;
+    font-family: 'Roboto';
+    
+
+    input {
+        width: 100%;
+        height: 75px;
+        background-color: #aaafff;
+        border: 0;
+        padding: 0 10px;
+
+        display: flex;
+        flex-direction: column;
+        align-items: flex-end;
+        
+
+        font-size: 24px;
+        font-family: 'Roboto';
+        color: #fff;
+    }
+    `
