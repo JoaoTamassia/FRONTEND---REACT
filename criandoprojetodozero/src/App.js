8bb79d7c15cf8fcaf2843f1a14c43fea4475d390
JoaoTamassia <joaotamassia@alunos.utfpr.edu.br>
2025-01-07 15:15:43 -0300

    commit

---
+++ criandoprojetodozero/src/App.js
@@ -0,0 +1,41 @@
+import React, { useState, useEffect } from "react";
+
+const App = () => {
+
+    const [ usuario, setUsuario] = useState("");
+    const [count, setCount] = useState(0);
+    const [usuarios, setUsuarios] = useState(
+        ["Juan", "Pedro", "Pablo"]
+    );
+
+    const handleAddUser = () => {
+        setUsuarios([...usuarios, usuario]);
+        setUsuario("");
+    }
+
+    useEffect(() => {
+        setCount(usuarios.length);
+    }, [usuarios])
+
+        return ( 
+            <div className="App">
+                <h1>Usuarios</h1>
+                <h3>Total: {count}</h3>
+                <div>
+                    <input
+                    value = {usuario}
+                    onChange={(event) => setUsuario(event.target.value)}
+                    />
+                    <button onClick={handleAddUser}>
+                        Adicionar
+                    </button>
+                </div>
+                <hr />
+                {usuarios.map((item) => (
+                    <p>{item}</p>
+                ))}
+            </div>
+        );
+}
+
+export default App;
