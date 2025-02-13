796fa9e303580773a16f2db1ae764ca31c18a633
JoaoTamassia <joaotamassia@alunos.utfpr.edu.br>
2025-02-13 15:45:58 -0300

    Criação tela home e tela login

--- criandoprojetocomreactcli/dio/src/pages/home/index.jsx
+++ criandoprojetocomreactcli/dio/src/pages/home/index.jsx
@@ -1,9 +1,33 @@
 import { Link } from "react-router-dom"
 
+import { Button } from "../../components/Button"
+import { Header } from "../../components/Header"
+import bannerImage from "../../assets/banner.png"
+
+import { Container, Title, TitleHighlight, TextContent } from "./styles"
+
 const Home = () => {
     return (<>
-        <h1>Home</h1>
-        <Link to="/login">Ir para a página de login</Link>
+        <Header />
+        <Container>
+            <div>
+                <Title>
+                    <TitleHighlight>
+                    Implemente
+                    <br />
+                    </TitleHighlight>
+                    o seu futuro global agora!
+                </Title>
+                <TextContent>
+                    Domine as tecnologias utilizadas pelas empresas mais inovadoras do mundo e encare seu novo
+                        desafio profissional, evoluindo em comunidade com os melhores experts.
+                </TextContent>
+                <Button title="Começar agora" variant="secondary"d onClick={() => null}/>
+            </div>
+            <div>
+                <img src={bannerImage} alt="Imagem Principal" />
+            </div>
+        </Container>
         </>
     )
 }
--- criandoprojetocomreactcli/dio/src/pages/login/index.jsx
+++ criandoprojetocomreactcli/dio/src/pages/login/index.jsx
@@ -1,9 +1,41 @@
 import { Link } from "react-router-dom"
 
+import { MdEmail, MdLock } from 'react-icons/md'
+
+import { Button } from "../../components/Button"
+import { Header } from "../../components/Header"
+import { Input } from "../../components/Input"
+
+import { Column, Container, CriarText, EsqueciText, Row, SubtitleLogin, Title, TitleLogin, Wrapper } from "./styles"
+
 const Login = () => {
     return (<>
-        <h1>Login</h1>
-        <Link to="/">Ir para a página inicial</Link>
+        <Header />
+        <Container>
+            <Column>
+                <Title>
+                    A plataforma para você aprender com experts, dominar as principais tecnmologias 
+                     e entrar mais rápido nas empresas mais desejadas.
+                </Title>
+            </Column>
+            <Column>
+                <Wrapper>
+                    <TitleLogin>Faça seu cadastro</TitleLogin>
+                    <SubtitleLogin>Faça seu login e make the change._</SubtitleLogin>
+                    <form>
+                        <Input placeholder="E-mail" leftIcon={<MdEmail/>}/>
+                        <Input placeholder="Senha" type="password" leftIcon={<MdLock/>}/>
+                        <Button title="Entrar" variant="secondary"/>
+                    </form>
+                    <Row>
+                        <EsqueciText>Esqueci minha senha</EsqueciText>
+                        <Link to="/register">
+                            <CriarText>Criar conta</CriarText>
+                        </Link>
+                    </Row>
+                </Wrapper>
+            </Column>
+        </Container>
         </>
     )
 }
